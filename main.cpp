889d167610034aca5fd2e87782c6fec58afe6b78
capitan <capitan@gmail.com>
2023-11-05 19:31:04 -0300

    for loop added

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-	cout << "start v.0.0.1" << endl;
+	cout << "version v.0.0.1" << endl;
+	cout << "start app" << endl;
+
+	for(int a = 0; a < 10; a++)
+	{
+		cout << "val: " << int(a) << endl;
+	}
 
 	return 0;
 }
