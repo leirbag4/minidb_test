56e047b0b73d1f880ed7404fbef4f3988b48dfb2
pepitoxt <pepito@gmail.com>
2023-11-07 15:17:34 -0300

    added binance price for btc

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@ using namespace std;
 
 Database db = Database();
 
+void doGet();
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
 int main()
 {
 	cout << "version v.0.0.2" << endl;
@@ -25,5 +33,28 @@ int main()
 
 	db.PrintAll();
 
+
+	doGet();
+
 	return 0;
+}
+
+void doGet()
+{
+  	CURL *curl;
+  	CURLcode res;
+  	std::string readBuffer;
+	
+	curl = curl_easy_init();
+  	
+  	if(curl) 
+  	{
+    	curl_easy_setopt(curl, CURLOPT_URL, "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
+    	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    	res = curl_easy_perform(curl);
+    	curl_easy_cleanup(curl);
+
+    	std::cout << readBuffer << std::endl;
+  	}
 }
